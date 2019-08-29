ef86587d4f7f03881732473c795bfc8a981d14d5
Amy Avirut <amyavirut@gmail.com>
2019-08-29 20:27:08 +0700

    060302

---
+++ src/components/Favorites.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Favorites.css'
+
+class Favorites extends React.Component {
+    render() {
+        return (<div className="favorites"></div>)
+    }
+}
+
+export default Favorites
