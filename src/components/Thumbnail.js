5bf3dff1e64db04eb79bb9d77e41281957d966e0
Amy Avirut <amyavirut@gmail.com>
2019-08-29 21:52:40 +0700

    060501

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -2,10 +2,21 @@ import React from 'react'
 import '../styles/Thumbnail.css'
 
 class Thumbnail extends React.Component {
+    state = {
+        liked: false
+    }
+
+    toggleLike = () => {
+        this.setState({
+           liked: !this.state.liked
+        })
+    }
+
     render() {
         return (
         <div className="thumbnail">
             {this.props.place.title} {this.props.place.price} {this.props.place.location}
+            <button onClick={this.toggleLike}>Like</button>
         </div>
         )
     }
