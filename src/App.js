38132124d176e374d470e9d91ccf44dc8deb5673
Amy Avirut <amyavirut@gmail.com>
2019-08-29 20:00:06 +0700

    060201

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Places from './components/Places.js';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1> 
+      <Places/>
     );
   }
 }
---
+++ src/components/Places.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Places extends React.Component {
+    state = {
+         places: [
+            {title: 'Banyan Tree', price: 5000 , location: 'Samui'}, 
+            {title: 'Intercon', price:4000 , location: 'Hua Hin'},
+            {title: 'Sala', price: 3000, location: 'Ayuthaya'},
+            {title: 'Nikko', price: 2000 , location: 'Bangkok'},
+            {title: 'Marriott', price: 1000 , location: 'Phuket'},
+            {title: 'Conrad', price: 1500, location: 'Chiangmai'},
+        ]
+    }
+    render() {
+        return ""
+    }
+}
+
+export default Places;
