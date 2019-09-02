fac591bae93cf8ccce6a800f2e71432694232e01
Amy Avirut <amyavirut@gmail.com>
2019-09-02 23:27:20 +0700

    060901

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import '../styles/Place.css'
 
+// The point components is to "encapsulate" - deal with smaller chunks
+// of HTML + JS + CSS. Letting us split the application up into smaller
+// reusable parts (components)
+// Components always have atleast one function, render()
+// The job render() is to use JSX to return a chunk HTML to be inserted into the DOM
+// Components can get data to put into JSX/DOM from 2 places
+// props - which are passed in when the component is created as JSX attributes
+// state - which is managed inside of the component itself, using methods like setState
+// Components can be nested inside of each other, we call this parent -> child relationship
+// The parent is able to pass props to the child via JSX attributes as previously mentioned
 class Place extends React.Component {
+    state = {
+        images: [
+            'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
+            'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg',
+            'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223174.jpg',
+            'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223178.jpg'
+        ]
+    }
+    
+
+    // render() returns JSX
+    // in JSX we can create HTML-like elements with the <element></element> syntax
+    // We can use JS by wrapping JS statements with {}, the return value of the expression is inserted into the JSX
+    // If you return an array of JSX elements then they are expanded automatically by JSX
+    // i.e {this.state.images.map(i => <Image image=i />)} would return an Array of JSX elements, specifically Image components
+    // Certain reserved JS words can't be used, mainly "class", so when setting the CSS class of a JSX element you must use className
+    // As an additional constraint render() must return a single parent JSX element that all other elements are children of.
+    // i.e you must return <div id=parent><div id=child></div></div> and NOT <div id=one></div><div id=two></div>
     render() {
         return (
             <div>
                 <h1>Next Destination One Click</h1>
+                <div className='main'>
+                    <div className='largeimage' style={{backgroundImage: `url('${this.state.images[0]}')` }}></div>
+                </div>
+                <div className='gallery'>
+                    {this.state.images.map((image, index) =>
+                        <div className="smallerimages" style={{backgroundImage: `url('${image}')` }} key={index} />
+                    )}
+                </div>
             </div>
         )
     }
